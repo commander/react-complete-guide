ad4ec7b39af696c25cdad961e441a7198562defd
Ashish Sheth <ashishsheth@gmail.com>
2019-06-24 09:18:36 +0530

    applied react-scripts eject. Applying component-wise style using react configuration.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -77,19 +77,19 @@ class App extends Component {
       style.backgroundColor = 'red';
     }
 
-    const classes = [];
+    let assignedClasses = [];
     if(this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses = assignedClasses.concat(classes.Red);
     }
 
     if(this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses = assignedClasses.concat(classes.Bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I am a React App</h1>
-        <p className={classes.join(' ')}>This is really working!!!</p>
+        <p className={assignedClasses.join(' ')}>This is really working!!!</p>
         <button style={style} 
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
@@ -97,4 +97,5 @@ class App extends Component {
     );
   }
 }
+
 export default App;
