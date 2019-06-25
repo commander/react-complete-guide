02754c3b5f1e078df12112cc413b3014f5535653
Ashish Sheth <ashishsheth@gmail.com>
2019-06-25 09:46:51 +0530

    Styling using css. Completed module 5

--- src/App.js
+++ src/App.js
@@ -49,16 +49,8 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
+    let btnClass = '';
 
     if(this.state.showPersons) {
       persons = (
@@ -74,7 +66,7 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
     }
 
     let assignedClasses = [];
@@ -90,7 +82,8 @@ class App extends Component {
       <div className={classes.App}>
         <h1>Hi, I am a React App</h1>
         <p className={assignedClasses.join(' ')}>This is really working!!!</p>
-        <button style={style} 
+        <button
+          className={btnClass} 
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
       </div>
